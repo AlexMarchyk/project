12660d13ac2a5ad085ce46d0f6524e382e21fa2a
AlexMarchyk <alexmarcyk@gmail.com>
2021-08-28 12:30:45 +0300

    int js

--- main.js
+++ main.js
@@ -9,3 +9,27 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
+
+const adv = document.querySelectorAll('.promo__adv');
+const poster = document.querySelector('.promo__bg');
+const genre =  poster.querySelector('.promo__genre');
+const movieList = document.querySelector('.promo__interactive-list');
+
+
+adv.forEach(function(item){
+ item.remove();
+});
+genre.textContent = "драма";
+
+poster.style.backgroundImage='url("/img/bg.jpg")';
+
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach(function(item,i){
+    console.log(item);
+    movieList.innerHTML += `<li class="promo__interactive-item">${i + 1} ${item}
+        <div class="delete"></div>
+    </li>`;
+});
